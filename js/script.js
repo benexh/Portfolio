97b6a5badb6fe1295a2174c0317fbae3f15bf66f
benexh <77386712+benexh@users.noreply.github.com>
2024-12-24 10:41:21 +0100

    GallerySetupV2

--- js/script.js
+++ js/script.js
@@ -10,4 +10,32 @@ window.addEventListener('mousemove', (e) => {
 
 });
 
+const stickySections =  [...document.querySelectorAll('.sticky')];
+let images = [
+    './icons/imgs/CoverDocV1.png',
+    './icons/imgs/CoverDocV1.png',    
+    './icons/imgs/CoverDocV1.png',    
+    './icons/imgs/CoverDocV1.png'    
+]
+ 
+images.forEach(img => {
+    stickySections.forEach(section => {
+        let image = document.createElement('img');
+        image.src =  img;
+        section.querySelector('.scroll-section').appendChild(image); 
+    }); 
+});
 
+window.addEventListener('scroll', (e) => {
+    for (let i = 0; i < stickySections.length; i++) {
+        transform(stickySections[i])
+    }
+})
+
+function transform(section){
+    const offsetTop = section.parentElement.offsetTop;
+    const scrollSection = document.querySelector('.scroll-section');
+    let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100; 
+    percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage; 
+    scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0  )`
+}
