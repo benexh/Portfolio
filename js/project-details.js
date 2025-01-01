0127a6c5cbc00a09cc8f036fc4b2eadb748481f3
benexh <77386712+benexh@users.noreply.github.com>
2025-01-01 12:32:53 +0100

    ProjectPageDummy

---
+++ js/project-details.js
@@ -0,0 +1,11 @@
+const cursor = document.querySelector('.cursor');
+
+window.addEventListener('mousemove', (e) => {
+    
+    const x = e.clientX;
+    const y = e.clientY;
+
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+
+});
